9d1ad23e7a98003485f76ab67f725c9e2e4cf987
김정민 <raon37910@gmail.com>
2025-02-10 14:27:19 +0900

    :heavy_plus_sign: Spring MVC, Thymeleaf, BootStrap 환경 세팅

---
+++ java-kata-web/src/main/java/io/kata/java/JavaKataWebApplication.java
@@ -0,0 +1,11 @@
+package io.kata.java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaKataWebApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(JavaKataWebApplication.class, args);
+	}
+}
