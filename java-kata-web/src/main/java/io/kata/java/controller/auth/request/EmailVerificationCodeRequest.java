215a2b93291ecce2fd77a4168bae676d9677ef2f
김정민 <raon37910@gmail.com>
2025-02-14 16:15:16 +0900

    [add] 이메일 인증 API 구현

---
+++ java-kata-web/src/main/java/io/kata/java/controller/auth/request/EmailVerificationCodeRequest.java
@@ -0,0 +1,11 @@
+package io.kata.java.controller.auth.request;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
+public record EmailVerificationCodeRequest(
+	@NotBlank(message = "이메일은 필수 값입니다.")
+	@Email(message = "이메일 형식으로 입력 해주세요.")
+	String email
+) {
+}
