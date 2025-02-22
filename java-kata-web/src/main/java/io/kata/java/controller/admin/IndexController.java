202fdd4cb26142d60d5f455cd02658a49e70631c
김정민 <raon37910@gmail.com>
2025-02-23 07:56:44 +0900

    [add] admin index page ui 개발

---
+++ java-kata-web/src/main/java/io/kata/java/controller/admin/IndexController.java
@@ -0,0 +1,13 @@
+package io.kata.java.controller.admin;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+
+	@GetMapping("/admin/index")
+	public String index() {
+		return "admin/index";
+	}
+}
