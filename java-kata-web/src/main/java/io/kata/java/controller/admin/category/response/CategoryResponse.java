371be50f405b0805cce1400afee24e3de2db63be
김정민 <raon37910@gmail.com>
2025-02-23 14:17:26 +0900

    [add] admin category index page ui 개발 + 카테고리 리스트 조회 기능 개발

---
+++ java-kata-web/src/main/java/io/kata/java/controller/admin/category/response/CategoryResponse.java
@@ -0,0 +1,12 @@
+package io.kata.java.controller.admin.category.response;
+
+/**
+ * @author    : kimjungmin
+ * Created on : 2025. 2. 23.
+ */
+public record CategoryResponse(
+	Long id,
+	String name,
+	String description
+) {
+}
